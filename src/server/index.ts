b859c84255de9876667260eb522d264b16399118
Vladimir Rujević <vladimir.rujevic@live.com>
2025-10-13 16:33:17 +0200

    Connect to Rabbit MQ
    - add missing convenience scripts to the package.json
    - set up start-up and shut-down scripts for the Peril server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,20 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const connection_string = "amqp://guest:guest@localhost:5672/";
+  const connection = await amqp.connect(connection_string);
+  console.log("Connected to RabbitMQ");
+  process.on("SIGINT", async () => {
+      try {
+          console.log("Closing RabbitMQ connection...")
+          await connection.close();
+      } catch (err) {
+          console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+          process.exit(0);
+      }
+  })
 }
 
 main().catch((err) => {
