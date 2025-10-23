a34c124b389571603b0a2ae148d7936e1441d7e7
Vladimir Rujević <vladimir.rujevic@live.com>
2025-10-23 20:32:18 +0200

    Implement REPL in the Peril client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,10 @@
 import amqp from "amqplib";
-import {clientWelcome} from "../internal/gamelogic/gamelogic.js";
+import {clientWelcome, commandStatus, getInput, printClientHelp, printQuit} from "../internal/gamelogic/gamelogic.js";
 import {declareAndBind} from "../internal/pubsub/index.js";
 import {ExchangePerilDirect, PauseKey} from "../internal/routing/routing.js";
+import {GameState} from "../internal/gamelogic/gamestate.js";
+import {commandSpawn} from "../internal/gamelogic/spawn.js";
+import {commandMove} from "../internal/gamelogic/move.js";
 
 async function main() {
     console.log("Starting Peril client...");
@@ -11,6 +14,32 @@ async function main() {
     const username = await clientWelcome();
     const [channel, queue] = await declareAndBind(connection, ExchangePerilDirect, `pause.${username}`, PauseKey, 'transient');
 
+    const gameState = new GameState(username);
+
+    while (true) {
+        const words = await getInput("Peril> ");
+        const command = words[0];
+
+        if (command === "spawn") {
+            commandSpawn(gameState, words);
+        } else if (command === "move") {
+            try {
+                commandMove(gameState, words);
+            } catch (err) {
+                console.error("Move failed:", err);
+            }
+        } else if (command === "status") {
+            await commandStatus(gameState);
+        } else if (command === "help") {
+            printClientHelp();
+        } else if (command === "quit") {
+            printQuit();
+            await connection.close();
+            process.exit(0);
+        } else {
+            console.log("Sorry, I don't understand that command.");
+        }
+    }
 }
 
 main().catch((err) => {
