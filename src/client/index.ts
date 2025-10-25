2ccdece2669684651812ec468546ae9817b64ca1
Vladimir Rujević <vladimir.rujevic@live.com>
2025-10-25 13:29:12 +0200

    Move pause handler to a separate handlers file

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,18 +1,11 @@
 import amqp from "amqplib";
 import {clientWelcome, commandStatus, getInput, printClientHelp, printQuit} from "../internal/gamelogic/gamelogic.js";
-import {declareAndBind, subscribeJSON} from "../internal/pubsub/index.js";
+import {subscribeJSON} from "../internal/pubsub/index.js";
 import {ExchangePerilDirect, PauseKey} from "../internal/routing/routing.js";
-import {GameState, type PlayingState} from "../internal/gamelogic/gamestate.js";
+import {GameState} from "../internal/gamelogic/gamestate.js";
 import {commandSpawn} from "../internal/gamelogic/spawn.js";
 import {commandMove} from "../internal/gamelogic/move.js";
-import {handlePause} from "../internal/gamelogic/pause.js";
-
-function handlerPause(gs: GameState) {
-    return (ps: PlayingState) => {
-        handlePause(gs, ps);
-        process.stdout.write("Peril> ");
-    }
-}
+import {handlerPause} from "./handlers.js";
 
 async function main() {
     console.log("Starting Peril client...");
@@ -41,8 +34,10 @@ async function main() {
         } else if (command === "move") {
             try {
                 commandMove(gameState, words);
-            } catch (err) {
-                console.error("Move failed:", err);
+            } catch (err: unknown) {
+                if (err instanceof Error) {
+                    console.error("Move failed:", err.message);
+                }
             }
         } else if (command === "status") {
             await commandStatus(gameState);
